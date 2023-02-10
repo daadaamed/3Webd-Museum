04c13dca88298c7c26736f3e63681ec2c39647b5
daadaamed <daadaamed@gmail.com>
2023-02-10 01:50:15 +0100

    search input gives you result for random articles for that search

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,35 +14,40 @@ function Nav(textInInput) {
   const [isLoading, setIsLoading] = useState(true);
   const textInput = JSON.stringify(textInInput.textInInput);
   console.log(`in Nav, text input ${textInput}`); // (result.objectId)
-  let url = `https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&isImage=true&q=sunflowers`;
+  let url = `https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&isImage=true&q=sun`;
   if (textInput.length == 2) {
     console.log(
       `empty as length = ${textInput.length}, so url=${url} and the textInput is ${textInput}`
     );
   } else {
-    url = `https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&isImage=true&q=mohamed`;
-    console.log(`non empty cause length = ${textInput.length}`);
+    url = `https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&isImage=true&q=${textInput}`;
+    //console.log(`non empty cause length = ${textInput.length}`);
   } // when empty, length is equal to 2
 
   useEffect(() => {
     async function fetchData() {
       console.log(`${url}`);
       let request = await axios.get(url);
-      setArticles(request.data.objectIDs);
       setIsLoading(false);
+      setArticles(request.data.objectIDs);
       return request;
     }
     setArticles((articles) => []);
+    //setIsLoading(true);
     fetchData();
   }, [textInput]);
 
   return (
     <div className="nav">
-      <h2> Welcome to Met Museum!</h2>
+      <h2>
+        {' '}
+        Welcome to Met Museum!<p>scroll right to discover more</p>
+      </h2>
 
       <div className="nav__posters">
         {isLoading ? (
           <img
+            className="loading"
             src={
               'https://i.pinimg.com/originals/3e/f0/e6/3ef0e69f3c889c1307330c36a501eb12.gif'
             }
